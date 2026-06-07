7bb86ba55e5218a682447c01a6d0ed0c4aca4606
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2026-06-07 02:27:17 +0200

    Grand reset

--- src/test/msw-setup.ts
+++
@@ -1,9 +0,0 @@
-import { setupServer } from "msw/node";
-import { beforeAll, afterEach, afterAll } from "vitest";
-import { llmHandlers } from "./handlers";
-
-export const server = setupServer(...llmHandlers);
-
-beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
